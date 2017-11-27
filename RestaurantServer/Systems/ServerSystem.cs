4bcae1b3de2e42e708ec6c4353538366303b18ba
aeklundh <eklundh.albin@gmail.com>
2017-11-27 13:45:58 +0100

    Add dish seeding for server

--- RestaurantServer/Systems/ServerSystem.cs
+++ RestaurantServer/Systems/ServerSystem.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using RestaurantLib;
+using RestaurantServer.Utilities;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +8,11 @@ namespace RestaurantServer.Systems
 {
     internal class ServerSystem
     {
+        private readonly List<Dish> _dishes;
+
+        public ServerSystem()
+        {
+            _dishes = SerializationUtility.ReadDishes();
+        }
     }
 }
--- RestaurantServer/Utilities/SerializationUtility.cs
+++ RestaurantServer/Utilities/SerializationUtility.cs
@@ -1,6 +1,27 @@
-﻿namespace RestaurantServer.Utilities
+﻿using Newtonsoft.Json;
+using RestaurantLib;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+
+namespace RestaurantServer.Utilities
 {
-    internal class SerializationUtility
+    internal static class SerializationUtility
     {
+        internal static List<Dish> ReadDishes()
+        {
+            string filePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + @"\Data\dishes.json";
+            if (File.Exists(filePath))
+            {
+                using (StreamReader reader = new StreamReader(filePath))
+                {
+                    return JsonConvert.DeserializeObject<List<Dish>>(reader.ReadToEnd());
+                }
+            }
+            else
+            {
+                return new List<Dish>();
+            }
+        }
     }
 }
