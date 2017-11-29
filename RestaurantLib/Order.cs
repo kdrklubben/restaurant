e089f1da3d817a79f06bce7e04833871d8e368fd
aeklundh <eklundh.albin@gmail.com>
2017-11-29 14:21:23 +0100

    Made Order shared model *Also add OrderId prop

---
+++ RestaurantLib/Order.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestaurantLib
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public Dish Dish { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
