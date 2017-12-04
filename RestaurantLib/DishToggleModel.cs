4ba7be8edff6de8584c3f6dcda02565fab0988e2
aeklundh <eklundh.albin@gmail.com>
2017-12-04 10:15:08 +0100

    Add DishToggleModel for toggling dishes with SETAVAILABLE command

---
+++ RestaurantLib/DishToggleModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestaurantLib
+{
+    public class DishToggleModel
+    {
+        public int DishId { get; set; }
+        public bool IsAvailable { get; set; }
+    }
+}
